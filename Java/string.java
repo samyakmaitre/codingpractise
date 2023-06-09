1a8492226199a27ae9ea27f0b3200d68a74b393d
Samyak8303 <pixelpirate404@gmail.com>
2023-06-09 21:26:45 +0530

    ugcfg

---
+++ Java/string.java
@@ -0,0 +1,14 @@
+package Java;
+
+public class string {
+    public static void main(String[] args) {
+        String s1 = "Computer Science";
+        String s2 = "VIT";
+        // int l1 = s2.length();
+        // String s2 = s2.toUpperCase(null);
+        // System.out .println(l1);
+        boolean variable = s2.contains("VIT ");
+        System.out.println(variable);
+    }
+    
+}
