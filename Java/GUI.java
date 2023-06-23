355755a7bd9e8d959b6524ad641fde4f48d07558
Samyak8303 <pixelpirate404@gmail.com>
2023-06-23 09:18:44 +0530

    function and table

---
+++ Java/GUI.java
@@ -0,0 +1,11 @@
+package Java;
+
+import javax.swing.JOptionPane;
+
+public class GUI{
+    public static void main (String[] args)
+    {
+        String name = JOptionPane.showInputDialog("Enter your name:");
+        JOptionPane.showMessageDialog(null,"Hello "+name);
+    }
+}
