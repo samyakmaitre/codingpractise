5f930905dadbeed3aec5d2b19488f68c28c6ed0a
Samyak8303 <pixelpirate404@gmail.com>
2023-07-09 23:51:26 +0530

    sdcsa

---
+++ Java/new.java
@@ -0,0 +1,20 @@
+import JAVA;
+import java.util.*;
+
+public class new{
+    public static void main(String[] args){
+        System.out.println("Helloworld")
+    }
+}
+
+
+// overview of solution
+// process flow
+// Please mention your Tech Stack (wherever applicable)
+// Detailed Description of the solution(Architecture)
+// Data Flow Diagram
+// how is your solution different?(USP of your approach)
+// future possible enhancements
+// Risk/Challenges/Dependencies
+// Acceptance Criteria Coverage(How many aspects of the problem statement have been covered?)
+// Additional info
