bc8568f970234cfab6c7472aabe62880d175ba95
Samyak Maitre <pixelpirate404@gmail.com>
2023-08-29 21:51:20 +0530

    fundamental

--- Python/arithematic operators.py
+++ Python/arithematic operators.py
@@ -4,5 +4,9 @@
 
 # print(age1+age2, age1-age2, age1*age2, age1/age2, age1%age2)
 
-First1name, Last1name = "Avi", "Jain"
-print(First1name+Last1name*10+"\n")
+# First1name, Last1name = "Avi", "Jain"
+# print(First1name+Last1name*10+"\n")
+
+
+state= "avi was playing basketball"
+print(state[1:2])
