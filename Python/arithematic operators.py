0fe0cd7dd722de41611295c3469ab09c2a4a9994
Samyak Maitre <pixelpirate404@gmail.com>
2023-08-29 21:33:41 +0530

    jhf

--- Python/arithematic operators.py
+++ Python/arithematic operators.py
@@ -1,5 +1,8 @@
-#Arithematic operators
+# #Arithematic operators
 
-age1, age2 = 14, 6
+# age1, age2 = 14, 6
 
-print(age1+age2, age1-age2, age1*age2, age1/age2, age1%age2)
+# print(age1+age2, age1-age2, age1*age2, age1/age2, age1%age2)
+
+First1name, Last1name = "Avi", "Jain"
+print(First1name +" "+ Last1name)
