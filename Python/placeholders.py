7fd5cbe7eabcd1229bd25c123101ff7e7e56822f
Samyak Maitre <pixelpirate404@gmail.com>
2023-08-30 10:21:09 +0530

    cgng

--- Python/placeholders.py
+++ Python/placeholders.py
@@ -2,6 +2,5 @@
 # automatic email system
 
 name = "Jake"
-namee= "jack"
-sentence= "%s is 15 years old"
-print(sentence % ("Barak")) 
+sentence= "%s %d is 15 years old"
+print(sentence % ("Barak", 23)) 
