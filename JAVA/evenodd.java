ba9313d56a87bc1c88616361ead3fbe82f34b756
samyakmaitre <pixelpirate404@gmail.com>
2023-10-17 23:39:00 +0530

    cahnge

--- JAVA/evenodd.java
+++ JAVA/evenodd.java
@@ -5,6 +5,7 @@ public class evenodd {
         System.out.println("Enter a number:");
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
+        
         System.out.println((a % 2 == 0) ? "Even" : "Odd");
         sc.close();
     }
