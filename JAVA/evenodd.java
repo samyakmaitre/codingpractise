7ee9595db502bb62449065dfb0056d52f75f38b6
samyakmaitre <pixelpirate404@gmail.com>
2023-10-25 12:02:30 +0530

    reuploaded JAVA

--- JAVA/evenodd.java
+++ JAVA/evenodd.java
@@ -4,8 +4,7 @@ public class evenodd {
     public static void main(String[] args) {
         System.out.println("Enter a number:");
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        
+        int a = sc.nextInt(); 
         System.out.println((a % 2 == 0) ? "Even" : "Odd");
         sc.close();
     }
