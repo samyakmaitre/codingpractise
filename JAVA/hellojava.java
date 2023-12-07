0316eaa5a1978938c5d44441f8989b0b0c4fd7f7
samyakmaitre <pixelpirate404@gmail.com>
2023-12-07 16:48:39 +0530

    java practise

---
+++ JAVA/hellojava.java
@@ -0,0 +1,12 @@
+imort java.util.*;
+
+class hellojava {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++){
+            
+        }
+    }
+}
