cbe0482c87bc1a6f093d01e2260954689022d100
samyakmaitre <pixelpirate404@gmail.com>
2023-12-08 09:50:34 +0530

    gbdvc

--- JAVA/hellojava.java
+++ JAVA/hellojava.java
@@ -1,4 +1,4 @@
-imort java.util.*;
+import java.util.*;
 
 class hellojava {
     public static void main(String args[]){
@@ -7,6 +7,27 @@ class hellojava {
 
         for (int i = 0; i < t; i++){
             
+            try{
+                long x = sc.nextLong();
+                System.out.println(x + "can be fitted in:");
+
+                if (x >= Byte.MIN_VALUE && x <= Byte.MAX_VALUE)
+                System.out.println("it a byte");
+
+                if (x >= Short.MIN_VALUE && x <= Short.MAX_VALUE)
+                System.out.println("its a short");
+
+                if (x >= Long.MIN_VALUE && x <= Long.MAX_VALUE);
+                System.out.println("its a long");
+
+                if(x >= Integer.MIN_VALUE && x <= Integer.MAX_VALUE);
+                System.out.println("its a integer");
+
+            }
+            catch(Exception e) {
+                System.out.println(sc.next() + "Cant be fitted anywhere.");
+            }
+
         }
     }
 }
