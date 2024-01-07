c370a48a81752785b9af2c06abbc986307985cc6
samyakmaitre <pixelpirate404@gmail.com>
2024-01-07 22:55:57 +0530

    kjdfs

--- JAVA/helloworld.java
+++ JAVA/helloworld.java
@@ -1,6 +1,12 @@
+import java.util.*;
 
-public class helloworld {
-    public static void main(String[] args) {
-        System.out.println("Helloo World");
+public class helloworld{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in) ;
+        int i = new.scanner
+        int j = 40;
+        int out = i/j;
+        System.out.println("Helloooo"+out);
+      
     }
 }
