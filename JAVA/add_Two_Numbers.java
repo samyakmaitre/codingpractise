9218c21177fee16f48630d7e9443e4af743e3b1c
samyakmaitre <pixelpirate404@gmail.com>
2024-01-11 23:41:04 +0530

    iuh

--- JAVA/add_Two_Numbers.java
+++ JAVA/add_Two_Numbers.java
@@ -3,9 +3,19 @@ import java.util.*;
 public class add_Two_Numbers{
     public static void main(String args[]){
         Scanner scn = new Scanner(System.in);
-        int a = scn.nextInt();
-        int b = scn.nextInt();
-        int sum = a + b;
-        System.out.println(sum + " is the sum of " + a +" & "+ b);
+        System.out.println("Enter r for radius and d for diameter:");
+        String a = scn.next();
+        if(a.equals("r")){
+            System.out.println("enter the radius:");
+            float r = scn.nextFloat();
+            double area = 3.14*(Math.pow(r,2));
+            System.out.println("area is "+area);
+        }
+        else{
+            System.out.println("enter the diameter:");
+            int d = scn.nextInt();
+            double are = 3.14*(Math.pow((d/2),2));
+            System.out.println("area is "+are);
+        }
     }
 }
