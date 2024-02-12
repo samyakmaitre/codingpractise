4c19e3c33624a8f4297e30c92c1166ef3e877e53
samyakmaitre <pixelpirate404@gmail.com>
2024-02-12 21:45:37 +0530

    ht

--- Python/placeholders.py
+++ Python/placeholders.py
@@ -1,12 +1,26 @@
-def transform_string(s):
-    result = ""
-    for i in range(len(s)):
-        if i % 2 == 0:  # even position
-            result += s[i].lower()
-        else:  # odd position
-            result += s[i].upper()
-    return result
+# def transform_string(s):
+#     result = ""
+#     for i in range(len(s)):
+#         if i % 2 == 0:  # even position
+#             result += s[i].lower()
+#         else:  # odd position
+#             result += s[i].upper()
+#     return result
 
-user_input = input("Enter a string: ")
-transformed_string = transform_string(user_input)
-print("Transformed string:", transformed_string)
+# user_input = input("Enter a string: ")
+# new_string = transform_string(user_input)
+# print("Transformed string:", new_string)
+
+# num = int(input())
+# doubled = num*2
+# print(doubled)
+
+
+# num1 = int(input())
+# num2 = int(input())
+# new = float(num1*num2)
+# print("The product of the number is :" + new)
+
+
+# radix sort
+# bucket sort
