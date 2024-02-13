936a7b9f4c20d87769d70683af873c1e317f8b83
samyakmaitre <pixelpirate404@gmail.com>
2024-02-14 00:00:22 +0530

    grd

---
+++ JAVA/java18.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class java18 {
+
+
+    public static int printhw(int a, int b) {
+        int sum = a + b ;
+        return sum;
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        printhw(x, y);
+        
+
+
+    }
+    
+}
