f763d8ce5915d259372fbb010e6d245e6f93c13d
samyakmaitre <pixelpirate404@gmail.com>
2024-02-14 23:32:08 +0530

    jyu

--- JAVA/java18.java
+++ JAVA/java18.java
@@ -3,19 +3,31 @@ import java.util.*;
 public class java18 {
 
 
+    // public static int printhw(int a, int b) {
+    //     int sum = a + b ;
+    //     return sum;
+    // }
+
+    // public static void main(String args[]){
+    //     Scanner sc = new Scanner(System.in);
+    //     int x = sc.nextInt();
+    //     int y = sc.nextInt();
+    //     printhw(x, y);
+        
+
     public static int printhw(int a, int b) {
-        int sum = a + b ;
+        int sum = a + b;
         return sum;
     }
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers:");
         int x = sc.nextInt();
         int y = sc.nextInt();
-        printhw(x, y);
-        
+        int result = printhw(x, y);
+        System.out.println("The sum is: " + result);
+    }
 
 
     }
-    
-}
