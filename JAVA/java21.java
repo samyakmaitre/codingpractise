dc592c8e0129c0df941e9596d70d4e4cfa595139
samyakmaitre <pixelpirate404@gmail.com>
2024-03-02 14:35:10 +0530

    java GFG

---
+++ JAVA/java21.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class java21{
+    public static void main(String[] args) {
+        int array[] = {1,2,3,4,5};
+        int sum = 0;
+        for(int mus : array){
+            sum += mus;
+        }
+    }
+}
+
+
+
+// for (int num : array) {
+//     arraySum += num;
+// }
