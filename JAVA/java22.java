590145ee831a3bef126e7129c48abdf0e1d3a23e
samyakmaitre <pixelpirate404@gmail.com>
2024-03-02 15:28:45 +0530

    funvtion practise

---
+++ JAVA/java22.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class java22{
+
+    public static int addition(int a, int b){
+
+    int c = a + b;
+    return c ;
+    }
+
+    public static void main (String args[]){
+        addition(10, 7);
+    }
+}
+
+// import java.util.*;
+
+// public class java22 {
+
+//     public static int addition(int a, int b) {
+//         int c = a + b;
+//         return c;
+//     }
+
+//     public static void main(String args[]) {
+    
+//         System.out.println(addition(10, 7));
+//     }
+// }
