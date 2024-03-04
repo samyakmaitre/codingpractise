d21b9306e08d7d1033303beead0443ef3c8c2104
samyakmaitre <pixelpirate404@gmail.com>
2024-03-04 12:09:41 +0530

    python

--- Python/variables copy.py
+++ Python/variables copy.py
@@ -5,12 +5,14 @@
 # print(b ,c ,d)
 
 
-x,y,z,f=27,10,-9,4
-a=x+y
-b=x-y
-c=x*y
-d=x/z
-e=x%y
-g=x**f
-h=x//y
-print(a,b,c,d,e,f,g,h)
+a = 13
+b = 17
+c = 12
+
+if a<b :
+    if a<c:
+        print("a is smallest")
+    else:
+        print("c is smallest")
+else:
+    print("b is smallest")
