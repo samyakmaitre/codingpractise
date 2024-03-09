ffeb720bdaa653736414e75584bdcdac0ae7db01
samyakmaitre <pixelpirate404@gmail.com>
2024-03-09 14:09:15 +0530

    qwedfg

--- Python/helloworld copy.py
+++ Python/helloworld copy.py
@@ -1,6 +1,15 @@
-word = "messi"
-L = len(word)
-N = word.len()
-print(L)
-print(N)
-print(len(word))
+# word = "messi"
+# L = len(word)
+# N = word.len()
+# print(L)
+# print(N)
+# print(len(word))
+
+# name  = "SaMyaK"
+# name = name.upper()
+# print(name)
+# name = name.lower()
+# print(name)
+
+name = "SaMyaK".lower()
+print(name)
