91f59c763217cfd9dda5f6e996d9433e44b9c1f7
samyakmaitre <pixelpirate404@gmail.com>
2024-03-09 23:15:28 +0530

    fgfhjkj

--- Python/helloworld copy.py
+++ Python/helloworld copy.py
@@ -11,5 +11,28 @@
 # name = name.lower()
 # print(name)
 
-name = "SaMyaK".lower()
-print(name)
+# name = "SaMyaK".lower()
+# print(name)
+
+# num1 = input(int())
+# num2 = input(int())
+# num1 = int(input())
+# num2 = int(input())
+
+# print(num1+num2)
+
+# num = input("Enter a number: ")
+# doubled = num * 3
+# print(doubled)
+
+# num1 = input()
+# num1 = int(num1)
+# num2 = float(num1)
+# print(num1,  " " ,num2)
+
+# top10 = "audi,merc,lambo,bmw,toyota,ferrari"
+# company = top10.split(",")
+# print(company)
+# for c in company:
+print(end="")
+    # print(c)
