1e81df3c101325b771a214eeb4ea644ae73826d3
samyakmaitre <pixelpirate404@gmail.com>
2024-04-11 23:24:59 +0530

    dssfg

---
+++ leetcode/enw/.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] sortedNums = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(sortedNums);
+        int left = 0;
+        int right = sortedNums.length - 1;
+        while (left < right) {
+            int sum = sortedNums[left] + sortedNums[right];
+            if (sum == target) {
+                int[] indices = new int[2];
+                indices[0] = findIndex(nums, sortedNums[left]);
+                indices[1] = findIndex(nums, sortedNums[right]);
+                return indices;
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[0];
+    }
+    
+    private int findIndex(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
