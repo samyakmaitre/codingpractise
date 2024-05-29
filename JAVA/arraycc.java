6d01f36f1c0b59a3b73f400e4e1caa2938e2f309
samyakmaitre <pixelpirate404@gmail.com>
2024-05-29 15:48:48 +0530

    trapped water interview question

--- JAVA/arraycc.java
+++ JAVA/arraycc.java
@@ -1,16 +1,31 @@
 public class arraycc {
 
-    public static int check(String arr[],String x){
-        for (int i=0;i<arr.length;i++){
-            if(arr[i]==x){
-                return i;
+    public static void printSA(int arr[])
+    {
+        int lrg = Integer.MIN_VALUE;
+        
+        for (int i=0;i<arr.length;i++)
+        {
+            for(int j = 0;j<arr.length;j++){
+                int curr = 0;
+                for(int k=i;k<=j;k++)
+                {   
+                    curr = curr + arr[k] ;
+                    if(curr>lrg){
+                        lrg = curr;
+                    }
+                    
+                }
+                System.out.print(curr+" ");
+                
             }
+            System.out.println();
         }
-        return -1;
+        System.out.println(lrg);
     }
 
     public static void main(String args[]){
-        String myarr[] = {"apple","orange","banana","grapes"};
-        System.out.println(check(myarr, "banana"));
+        int myarr[] = {1,-2,6,-1,3};
+        printSA(myarr);
     }
 }
