b15e0ee09b48aac4ad9750e6bfb8317be8700153
samyakmaitre <pixelpirate404@gmail.com>
2024-07-10 00:49:53 +0530

    commit

---
+++ JAVA/T.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Map;
+
+class T {
+    public int[] twoSum(int[] nums, int target){
+        //create a hashmap to store the the diff of target and current number
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        //Interate over the array
+        for (int i=0; i<nums.length; i++){
+    
+            //if the complement is in the map, we found the solution
+            if(map.containsKey(target - nums[i])){
+                return new int[] {map.get(target - nums[i]),i};
+            }
+            //Otherwise, add the current number and its index to the map
+            map.put(nums[i],i);
+        }
+        
+        //in case of no solution
+        throw new IllegalArgumentException("No solution");
+    }
+}
