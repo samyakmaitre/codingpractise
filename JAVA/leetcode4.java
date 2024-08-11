3575ecbb5a2b6c7f25df5e2dd811bfffd387017b
samyakmaitre <pixelpirate404@gmail.com>
2024-08-11 12:44:18 +0530

    sorted folders

---
+++ JAVA/leetcode4.java
@@ -0,0 +1,17 @@
+public class leetcode4 {
+    public double findMed(int[] nums1, int[] nums2){
+        int n = nums1.length;
+        int m = nums2.length;
+        int i=0, j=0, m1=0, m2=0;
+
+        //median
+        for(int count = 0; count<= (n+m)/2; count++){
+            m2 = m1;
+            if(int )
+        }
+    }
+
+    public static void main(String[] args){
+
+    }
+}
