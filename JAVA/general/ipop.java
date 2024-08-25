eee25c6a1059515de3ebb9ed18484b99e0965966
samyakmaitre <pixelpirate404@gmail.com>
2024-08-25 15:10:22 +0530

    changes

--- JAVA/general/ipop.java
+++
@@ -1,5 +0,0 @@
-public class ipop{
-    public static void main(String[] args){
-        System.out.println("Samyak");
-    }
-}
--- JAVA/general/java3.java
+++
@@ -1,8 +0,0 @@
-
-
-public class java3{
-    public static void main(String args[]){
-        
-    }
-    
-}
