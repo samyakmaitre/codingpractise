91b2e13b43ff987e2ab8df18802ba2ff6544ad42
samyakmaitre <pixelpirate404@gmail.com>
2024-09-19 22:57:35 +0530

    TCS number system

---
+++ TCS100/stringBinToDecimal.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class stringBinToDecimal {
+    public static void main(String[] args){
+        String s = "1111";
+
+        int decimal = Integer.parseInt(s,2);
+
+        System.out.println(decimal);
+    }
+}
