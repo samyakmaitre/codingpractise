614cad45d986a3a98d75a17fa80d5c9ec5725a80
samyakmaitre <samyakcodes@gmail.com>
2024-12-23 07:42:50 +0530

    graph

--- JAVA/graph1.java
+++ JAVA/graph1.java
@@ -13,33 +13,36 @@ public class graph1{
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         int v = 5;
         ArrayList<Edge>[] graph = new ArrayList[v];
 
-        for(int i=0; i<v; i++){
+        // Initializing the adjacency list
+        for(int i = 0; i < v; i++){
             graph[i] = new ArrayList<>();
         }
 
-        graph[0].add(new Edge(0,1,5));
+        // Adding edges
+        graph[0].add(new Edge(0, 1, 5));
 
-        graph[1].add(new Edge(1,0,5));
-        graph[1].add(new Edge(1,2,1));
-        graph[1].add(new Edge(1,3,3));
+        graph[1].add(new Edge(1, 0, 5));
+        graph[1].add(new Edge(1, 2, 1));
+        graph[1].add(new Edge(1, 3, 3));
 
-        graph[2].add(new Edge(2,1,1));
-        graph[2].add(new Edge(2,3,1));
-        graph[2].add(new Edge(2,4,4));
-        
-        graph[3].add(new Edge(3,1,3));
-        graph[3].add(new Edge(3,2,1));
+        graph[2].add(new Edge(2, 1, 1));
+        graph[2].add(new Edge(2, 3, 3));
+        graph[2].add(new Edge(2, 4, 4));
 
-        graph[4].add(new Edge(4,2,2));
+        graph[3].add(new Edge(3, 1, 3));
+        graph[3].add(new Edge(3, 2, 1));
 
-        for(int i=0; i<graph[2].size(); i++){
+        graph[4].add(new Edge(4, 2, 2));
+
+        // Printing the weights of edges from vertex 2
+        System.out.print("Edge weights from vertex 2: ");
+        for(int i = 0; i < graph[2].size(); i++){
             Edge e = graph[2].get(i);
-            System.out.println(e.dest);
+            System.out.print(e.wt + " "); // Printing the edge weights
         }
-
     }
 }
