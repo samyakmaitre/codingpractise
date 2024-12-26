b99b03ea8138909a884e1df92b0511e276a61d1f
samyakmaitre <samyakcodes@gmail.com>
2024-12-26 08:03:44 +0530

    search

---
+++ JAVA/LinearSearch.java
@@ -0,0 +1,13 @@
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {3,2,5,1,2,9,8,4};
+
+        //linear search
+        for(int x : arr){
+            if(x == 2){
+                System.out.println("Found");
+            }
+        }
+    }
+}
--- JAVA/binary.java
+++ JAVA/binary.java
@@ -1,27 +1,30 @@
 public class binary{
-    public static void binary(int[] arr, int key){
-        int si = 0;
-        int ei = arr.length-1;
+    public static void binarysearch(int[] arr, int key){
+        int start = 0;
+        int end = arr.length -1;
 
-        while(si <= ei){
-            int mid = si + (ei - si)/2;
-            if(arr[mid]==key){
-                System.out.println("index is "+ mid);
-                return;
+        while(start<=end){
+            int mid = start + (end -start)/2 ;
+
+            if(arr[mid]==key) {
+                System.out.println("key found at: " + mid) 
+                return
             }
 
             if(arr[mid]<key){
-                si = mid+1;
+                start = mid + 1;
             }
-            if(arr[mid]>key){
-                ei = mid-1;
+
+            else{
+                end = mid - 1;
             }
         }
-        System.out.println("not found");
-    }
-    public static void main(String[] args){
-        int arr[] = {2, 4, 6, 8, 10, 12};
-        binary(arr, 11);
 
+        System.out.println("key not found");
+    }
+
+    public static void main(String[] args) {
+        int[] array = {2,4,6,8,10,12};
+        binarysearch(array, 11);
     }
 }
