eb4d9f1e306e61889a4f71b57a231a13f2b4b88a
samyakmaitre <samyakcodes@gmail.com>
2025-02-11 20:50:40 +0530

    check cycle

---
+++ JAVA/graphs/bipartite.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+public class bipartite{
+    static class Edge{
+        int src;
+        int dest;
+        int wt;
+
+        Edge(int s, int d, int w){
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }
+
+    public static boolean isBipartite(ArrayList<Edge>[] graph){
+        int col[] = new int [graph.length];
+        for(int i=0; i<col.length; i++){
+            col[i] = -1;
+        }
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i=0; i<graph.length; i++){
+            if(col[i] == -1){
+                q.add(i);
+                col[i] = 0;
+                while(!q.isEmpty()){
+                    int curr = q.remove();
+                    for(int j=0; j<graph[curr].size(); i++){
+                        Edge e = graph[curr].get(j);
+                        if(col[e.dest] == -1){
+                            int nextCol = col[curr] == 0 ? 1 : 0;
+                        }
+                    }
+                }
+            }
+        }
+        
+        return false;
+    }
+
+    public static void main(String[] args){
+        ArrayList<Edge>[] graph1 = new ArrayList[4];
+        ArrayList<Edge>[] graph2 = new ArrayList[5];
+
+        for(int i=0; i<graph1.length; i++){
+            graph1[i] = new ArrayList<>();
+        }
+        for(int i=0; i<graph2.length; i++){
+            graph2[i] = new ArrayList<>();
+        }
+
+        //graph1
+        graph1[0].add(new Edge(0,1,1));
+        graph1[0].add(new Edge(0,4,1));
+
+        graph1[1].add(new Edge(1,0,1));
+        graph1[1].add(new Edge(1,2,1));
+
+        graph1[2].add(new Edge(2,1,1));
+        graph1[2].add(new Edge(2,3,1));
+
+        graph1[3].add(new Edge(3,2,1));
+        graph1[3].add(new Edge(3,4,1));
+        
+        //graph2
+        graph2[0].add(new Edge(0,1,1));
+        graph2[0].add(new Edge(0,4,1));
+
+        graph2[1].add(new Edge(1,0,1));
+        graph2[1].add(new Edge(1,2,1));
+
+        graph2[2].add(new Edge(2,1,1));
+        graph2[2].add(new Edge(2,3,1));
+
+        graph2[3].add(new Edge(3,2,1));
+        graph2[3].add(new Edge(3,4,1));
+
+        graph2[4].add(new Edge(4,0,1));
+        graph2[4].add(new Edge(4,3,1));
+        
+        //
+    }
+}
--- JAVA/graphs/cycleDetection.java
+++ JAVA/graphs/cycleDetection.java
@@ -2,7 +2,9 @@ import java.util.*;
 
 public class cycleDetection{
     static class Edge{
-        int src, dest, wt;
+        int src;
+        int dest;
+        int wt;
 
         Edge(int s, int d, int w){
             this.src = s;
@@ -10,8 +12,8 @@ public class cycleDetection{
             this.wt = w;
         }
     }
-
-    public static boolean detectCycle(ArrayList<Edge>[] graph){
+    
+    public static boolean checkCycle(ArrayList<Edge>[] graph){
         boolean[] vis = new boolean[graph.length];
         for(int i=0; i<graph.length; i++){
             if(!vis[i]){
@@ -20,16 +22,16 @@ public class cycleDetection{
                 }
             }
         }
+
         return false;
     }
 
     public static boolean checkCycleUtil(ArrayList<Edge>[] graph, boolean[] vis, int curr, int par){
         vis[curr] = true;
-        
         for(int i=0; i<graph[curr].size(); i++){
-            Edge e = graph[curr].get(i);
+            Edge e = graph[curr].get(i); 
             if(!vis[e.dest]){
-                if(checkCycleUtil(graph, vis, e.dest, curr)){
+                if(!checkCycleUtil(graph, vis, e.dest, curr)){
                     return true;
                 }
             }
@@ -39,24 +41,39 @@ public class cycleDetection{
         }
         return false;
     }
+    
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         int v = 7;
         ArrayList<Edge>[] graph = new ArrayList[v];
-        for(int i=0;i<graph.length; i++){
-            graph[0].add(new Edge(0,1,1));
-            graph[0].add(new Edge(0,3,1));
 
-            graph[1].add(new Edge(1,0,1));
-            graph[1].add(new Edge(1,2,1));
-
-            graph[2].add(new Edge(2,0,1));
-            graph[2].add(new Edge(2,1,1));
-
-            graph[3].add(new Edge(3,1,1));
-            graph[3].add(new Edge(3,4,1));
-
-            graph[4].add(new Edge(4,3,1));
+        for(int i=0; i<v; i++){
+            graph[i] = new ArrayList<>();
         }
+
+        graph[0].add(new Edge(0, 1, 1));
+        graph[0].add(new Edge(0, 2, 1));
+
+        graph[1].add(new Edge(1, 0, 1));
+        graph[1].add(new Edge(1, 3, 1));
+
+        graph[2].add(new Edge(2, 0, 1));
+        graph[2].add(new Edge(2, 4, 1));
+
+        graph[3].add(new Edge(3, 1, 1));
+        graph[3].add(new Edge(3, 4, 1));
+        graph[3].add(new Edge(3, 5, 1));
+
+        graph[4].add(new Edge(4, 2, 1));
+        graph[4].add(new Edge(4, 3, 1));
+        graph[4].add(new Edge(4, 5, 1));
+
+        graph[5].add(new Edge(5, 3, 1));
+        graph[5].add(new Edge(5, 4, 1));
+        graph[5].add(new Edge(5, 6, 1));
+
+        graph[6].add(new Edge(6, 5, 1));
+
+        checkCycle(graph);
     }
 }
